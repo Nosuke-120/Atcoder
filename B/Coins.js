534f9f3d6ccf59613aeb05cb6db6c2e5ca01bd43
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-04-18 00:23:55 +0900

    modify: B/Coins.js

--- B/Coins.js
+++ B/Coins.js
@@ -1,13 +1,13 @@
 const main = (input) => {
   let inputValues = input.split("\n").map((val) => parseInt(val, 10));
   let result = 0;
+  let sum = 0;
 
   for(let i =0; i<=inputValues[0]; i++) {
     for(let j =0; j<=inputValues[1]; j++) {
       for(let k =0; k<=inputValues[2]; k++) {
-        if (500*i + 100*j + 50*k === inputValues[3]) {
-          result++
-        };
+        sum = 500*i + 100*j + 50*k;
+        if (sum == inputValues[3]) result++
       };
     };
   };
