ea316f7a97d2f29018b73cfd29c751078cf92c75
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-04-27 00:35:56 +0900

    modify: C/Traveling.js

--- C/Traveling.js
+++ C/Traveling.js
@@ -5,12 +5,12 @@ const main = (input) => {
   // let nums = args.slice(1, N+1).map(n => n.split(' '))
   list = [[0, 0, 0]];
     for (let i = 1; i <=n; i++) {
-      let t = Number(lines[i].split(" ")[0]);
-      let x = Number(lines[i].split(" ")[1]);
-      let y = Number(lines[i].split(" ")[2]);
+      let t = parseInt(lines[i].split(" ")[0], 10);
+      let x = parseInt(lines[i].split(" ")[1], 10);
+      let y = parseInt(lines[i].split(" ")[2], 10);
       list.push([t,x,y]);
     }
-    for (var i=0;i<n;i++){
+    for (let i = 0; i < n; i++){
       dt = list[i + 1][0] - list[i][0];
       dx = Math.abs(list[i + 1][1] - list[i][1]);
       dy = Math.abs(list[i + 1][2] - list[i][2]);
